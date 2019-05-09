efdcf9934f2048e3b31f238a7b3e566cd74c2482
jypark <bh9578@gmail.com>
2019-05-09 16:00:52 +0900

    주석 정리

--- src/main/resources/spring/TestWebApplicationInitializer.java
+++ src/main/resources/spring/TestWebApplicationInitializer.java
@@ -17,35 +17,26 @@ import spring.config.TestWebConfig;
 public class TestWebApplicationInitializer implements WebApplicationInitializer {
 
   private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
-  // private static final String SECURITY_SERVLET_NAME = "security";
 
   @Override
   public void onStartup(ServletContext servletContext) throws ServletException {
-    // servletContext.setInitParameter("key", "value");
     registerDispatcherServlet(servletContext);
   }
 
   private void registerDispatcherServlet(ServletContext servletContext) {
-
-    // 리스너 추가
-    // AnnotationConfigWebApplicationContext listenerContext = new
-    // AnnotationConfigWebApplicationContext();
-    // listenerContext.register(TestListenerConfig.class);
-    // servletContext.addListener(new ContextLoaderListener(listenerContext));
-
-    // 필터 추가
+    // Filter
     FilterRegistration.Dynamic characterEncodingFilterRegist =
         servletContext.addFilter("characterEncodingFilter", new CharacterEncodingFilter());
     characterEncodingFilterRegist.setInitParameter("encoding", "UTF-8");
     characterEncodingFilterRegist.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true,
         "/*");
 
-    // 필터 추가
+    // Filter
     FilterRegistration.Dynamic delegatingFilterRegist =
         servletContext.addFilter("springSecurityFilterChain", new DelegatingFilterProxy());
     delegatingFilterRegist.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
 
-    // 서블릿 추가
+    // Servlet
     AnnotationConfigWebApplicationContext dispatcherContext =
         new AnnotationConfigWebApplicationContext();
     dispatcherContext.register(TestWebConfig.class);
@@ -56,16 +47,5 @@ public class TestWebApplicationInitializer implements WebApplicationInitializer 
         new DispatcherServlet(dispatcherContext));
     dispatcherServletRegist.setLoadOnStartup(1);
     dispatcherServletRegist.addMapping("/");
-
-    // 서블릿 추가
-    // AnnotationConfigWebApplicationContext securityContext = new
-    // AnnotationConfigWebApplicationContext();
-    // securityContext.register(TestSecurityWebConfig.class);
-    //
-    // ServletRegistration.Dynamic securityServletRegist;
-    // securityServletRegist = servletContext.addServlet(SECURITY_SERVLET_NAME, new
-    // DispatcherServlet(securityContext));
-    // securityServletRegist.setLoadOnStartup(1);
-    // securityServletRegist.addMapping("/");
   }
 }
